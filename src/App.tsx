1408f77e1978f2fb464e0a4df8ac81da86d35977
Adriane Oliveira <165448523+adriolivdev@users.noreply.github.com>
2025-04-07 17:57:00 -0300

    fix:bug nas rotas pro vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Header from "./components/Header";
+import Welcome from "./pages/Welcome";
 import Home from "./pages/Home";
 
 /**
  * Componente principal do aplicativo.
- * Renderiza o cabeçalho, conteúdo principal e o rodapé.
+ * Define as rotas e renderiza o cabeçalho e conteúdo conforme a página.
  */
 export default function App() {
   return (
     <div className="min-h-screen flex flex-col bg-black text-white">
-      {/* Cabeçalho */}
-      <Header />
+      <Router>
+        {/* Cabeçalho fixo */}
+        <Header />
 
-      {/* Conteúdo principal */}
-      <main className="flex-1">
-        <Home />
-      </main>
+        {/* Rotas principais */}
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<Navigate to="/welcome" replace />} />
+            <Route path="/welcome" element={<Welcome />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </main>
+      </Router>
     </div>
   );
 }
